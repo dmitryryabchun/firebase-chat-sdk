f0fc6c3322785666ae49148b8b98a2c88cb25099
vitaliy.karpinets <vitalik.karpinets@gmail.com>
2022-11-25 18:26:31 +0200

    export function for updating message

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,4 @@ export { connectUser } from './user/user';
 export { subscribeUser, unsubscribeUser } from './user/user-collection';
 export { addUserToChannel, removeUserFromChannel, updateChannel, updateChannelFull } from './channel/channel';
 export { createChannel, getChannel, findChannelsByTags, findChannelsByUser, subscribeChannels, subscribeChannel, unsubscribeChannel } from './channel/channel-collection';
-export { postMessage, getMessages, subscribeMessage, unsubscribeMessage } from './message/message-collection';
+export { postMessage, getMessages, subscribeMessage, unsubscribeMessage, updateMessage } from './message/message-collection';
